becd068cc7dae271f4e5981b0151df878e613900
isabellegan <78586330+isabellegan@users.noreply.github.com>
2025-01-29 15:43:28 -0500

    installed dependencies, created env variables, and set up express server with socket

--- server.js
+++ server.js
@@ -1,24 +1,38 @@
-// index.js
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import http from "http";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import path from "path";
+const { Server } = require("socket.io");
+
+dotenv.config();
+const app = express();
+
+// Creating an HTTP server using an Express app
+const server = http.createServer(app);
+
+// Initialize Socket.io and attach it to the HTTP server
+const io = new Server(server, {
+  cors: {
+    origin: "*", // Allow connections from any origin (for development; pdate for production)
+  },
+});
+
+// Enable CORS for all routes, parse JSON request bodies, and use PORT from .env
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 8080;
 
 // Get current file's directory
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const PORT = process.env.PORT || 8081;
-const app = express();
-
 // Middleware for logging requests
 app.use((req, res, next) => {
   next();
 });
-
-app.use(cors());
-app.use(express.json());
 
 // Root route
 app.get("/", (req, res) => {
